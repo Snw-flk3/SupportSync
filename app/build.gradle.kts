a58b655dbb131e38ba0ec6295b181d8aaec32b53
NPMpayela <104700552+NPMpayela@users.noreply.github.com>
2024-11-18 02:11:26 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,13 +39,11 @@ android {
 
 dependencies {
     // Microsoft SQL Server JDBC Driver
-    implementation("com.microsoft.sqlserver:mssql-jdbc:9.2.1.jre11")
+    implementation("com.microsoft.sqlserver:mssql-jdbc:12.2.0.jre11")
 
-    // Image loading library (choose one)
+    // Image loading library
     implementation("com.github.bumptech.glide:glide:4.15.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.15.0")
-//Piscasso is commented out to avoid duplication with glide
-   //implementation("com.squareup.picasso:picasso:2.71828") // Uncomment if using Picasso
 
     // AndroidX Libraries
     implementation("androidx.core:core-ktx:1.13.1")
@@ -54,15 +52,14 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Firebase dependencies
+    implementation(platform("com.google.firebase:firebase-bom:32.1.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-storage-ktx")
 
-    implementation(platform("com.google.firebase:firebase-bom:32.1.0")) // Import the BoM to manage version
-    implementation("com.google.firebase:firebase-auth") // Firebase Authentication
-    implementation("com.google.firebase:firebase-database") // Firebase realtime database
-    implementation("com.google.firebase:firebase-firestore") //Firebase firestore
-    implementation("com.google.firebase:firebase-storage-ktx") // Firebase storage KTX extensions
-    implementation ("com.google.firebase:firebase-firestore-ktx:24.4.1") // Update to the latest version
-    implementation ("com.google.firebase:firebase-storage-ktx:20.1.0") // Update to the latest version
-    implementation ("androidx.appcompat:appcompat:1.5.1") // Ensure this is included
+    // Reactor Core (for reactive programming)
+    implementation("io.projectreactor:reactor-core:3.5.10")
 
     // Testing Libraries
     testImplementation("junit:junit:4.13.2")
